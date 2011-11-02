a4fba201e48df39a4f7f843814e23e8cf15dd771
Liag <liag247@gmail.com>
2011-11-02 18:00:07 +0100

    Added projects

---
+++ Snake/src/lab1/SnakeModel.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class SnakeModel extends GameModel
+{
+
+	@Override
+	public void gameUpdate(int lastKey) throws GameOverException
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
